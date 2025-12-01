94de1dcaa4283e0dc95a11e65c04a126087652a1
kwicxy <ones_republic@foxmail.com>
2025-12-01 12:05:24 +0800

    revert: pynput在Windows会出现bug，回退到keyboard库
    This reverts commit a6babc9e

--- src/tui.py
+++ src/tui.py
@@ -1,5 +1,5 @@
 """ Textual UI 版本"""
-from pynput.keyboard import GlobalHotKeys
+from pynput.keyboard import Key, Controller, GlobalHotKeys
 from sys import platform
 import os
 import yaml
@@ -19,10 +19,11 @@ PLATFORM = platform.lower()
 if PLATFORM.startswith('win'):
     try:
         import win32clipboard
+        import keyboard
         import win32gui
         import win32process
     except ImportError:
-        print("[red]请先安装 Windows 运行库: pip install pywin32[/red]")
+        print("[red]请先安装 Windows 运行库: pip install pywin32 keyboard[/red]")
         raise
 
 class ManosabaTUI(App):
@@ -59,12 +60,15 @@ class ManosabaTUI(App):
     def setup_global_hotkeys(self) -> None:
         """设置全局热键监听器"""
         keymap = self.keymap
-        hotkeys = {
-            keymap['start_generate']: self.trigger_generate
-        }
+        if PLATFORM == "darwin":
+            hotkeys = {
+                keymap['start_generate']: self.trigger_generate
+            }
 
-        self.hotkey_listener = GlobalHotKeys(hotkeys)
-        self.hotkey_listener.start()
+            self.hotkey_listener = GlobalHotKeys(hotkeys)
+            self.hotkey_listener.start()
+        elif PLATFORM.startswith('win'):
+            keyboard.add_hotkey(keymap['start_generate'], self.trigger_generate)
 
     def trigger_generate(self) -> None:
         """全局热键触发生成图片（在后台线程中调用）"""
